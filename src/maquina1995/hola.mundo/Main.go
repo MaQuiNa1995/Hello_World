768bad5061355a6a8a4a178cd177bc82b8b3030f
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-17 00:34:11 +0200

    Creación de wiki de Go y eliminación de código y explicaciones anteriores

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -1,57 +1,33 @@
-// Declaracion del paquete
 package main
 
-// Zona de imports
 import (
 	"fmt"
 )
 
-// Las declaraciones a nivel de paquete no permiten el operador :=
-
-var numero4 int
-var numero5 int = 5
-
-/*
-	Adicionalmente se permite la aglutinación de distintas variables
-	(que por buena practica deberían de guardar relacion entre ellas)
-	con el siguiente bloque
-*/
-var (
-	numero6 int
-	numero7 int = 7
-	// podemos tener el mismo nombre de variable pero en distintos "scopes"
-	numero int = 1
+const (
+	constanteEnumerada  = iota + 5 // valdría 5
+	constanteEnumerada2            // valdría 6
 )
 
-/* función main de la aplicación */
+const (
+	_  = iota
+	KB = 1 << (10 * iota) // Con esto conseguimos una potencia 10^1
+	MB                    // Con esto conseguimos una potencia 10^2 etc
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
 func main() {
-
-	// imprimir por pantalla
-	fmt.Println("Hola Mundo")
-
-	//Diferentes formas de declaración de variables
-	//var numero float32 = 1
-	//numero2 := 2
-	//var numero3 int
-	//numero3 = 3
-
-	var numero float32 = 1
+	// Aqui tenemos un decimal que representa el peso de un fichero
+	fileSize := 4000000000.
 	/*
-		El problema de esta declaracion es que nos inicializa la variable
-		con float64 no hay ninguna forma de crear un float32 por esta vía
-		ese sería el unico problema de esta forma
+		Con el Printf aplicamos cierto formato
+		%.2f -> decimal de precisión 2
+		concatenado con " GB"
 	*/
-	numero2 := 2.
-
-	// podemos aplicar cierto formato a la salida por consola
-	// %v -> value de la variable
-	// %T -> Tipo de la variable
-
-	/*
-		En este caso saldría 2 veces ya que tenemos 2 variables
-		que se llaman numero pero con distinto scope
-	*/
-	fmt.Printf("%v, %T ", numero, numero)
-	fmt.Printf("%v, %T", numero2, numero2)
-
+	fmt.Printf("%.2f GB", fileSize/GB) // Imprime 3.73 GB
 }
