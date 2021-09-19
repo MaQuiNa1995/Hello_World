7efa41fc6cffd61cf929597c0deb5fa7c339a531
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-19 02:37:52 +0200

    Añadido ejemplo de arrays, punteros de los mismos y nuevas entradas en la wiki, video 1:58:48

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -4,30 +4,18 @@ import (
 	"fmt"
 )
 
-const (
-	constanteEnumerada  = iota + 5 // valdría 5
-	constanteEnumerada2            // valdría 6
-)
-
-const (
-	_  = iota
-	KB = 1 << (10 * iota) // Con esto conseguimos una potencia 10^1
-	MB                    // Con esto conseguimos una potencia 10^2 etc
-	GB
-	TB
-	PB
-	EB
-	ZB
-	YB
-)
-
 func main() {
-	// Aqui tenemos un decimal que representa el peso de un fichero
-	fileSize := 4000000000.
-	/*
-		Con el Printf aplicamos cierto formato
-		%.2f -> decimal de precisión 2
-		concatenado con " GB"
-	*/
-	fmt.Printf("%.2f GB", fileSize/GB) // Imprime 3.73 GB
+
+	// copia nueva
+	profesiones := [...]string{"Caballero Cebolla", "Ilusionista", "Francotirador"}
+	profesionesCopia := profesiones
+	profesionesCopia[0] = "Mago Azul"
+	fmt.Printf("Profesiones:       %v\nProfesiones Copia: %v\n", profesiones, profesionesCopia)
+
+	// copia real apuntando a la misma dirección de memoria
+	profesiones2 := [...]string{"Caballero Cebolla", "Ilusionista", "Francotirador"}
+	profesionesCopia2 := &profesiones2
+	profesionesCopia2[0] = "Domador"
+	fmt.Printf("Profesiones:       %v\nProfesiones Copia: %v\n", profesiones2, profesionesCopia2)
+
 }
