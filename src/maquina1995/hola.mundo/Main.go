c779d6b2eaa26349acd52614725f5aeaa9195616
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-21 23:20:10 +0200

    Ejemplo de mapas y adicion de los mismos a la wiki

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	slice := []string{"Mago del Tiempo", "Monje", "Paladín", "Elementalista", "Esgrimista"}
-	// Crearíamos un nuevo slice omitiendo el último elemento del original
-	// slice[:2] -> crea un array con: Mago del Tiempo Monje
-	// slice[4:]... -> añade a ese array creado los elementos resultantes de descartar 4 elementos del slice original
-	slice2 := append(slice[:2], slice[4:]...)
-	// imprime: [Mago del Tiempo Monje Esgrimista]
-	// se han eliminado los elementos centrales: Paladín y Elementalista
-	fmt.Print(slice2)
+	profesiones := map[int]string{
+		675: "Soldado",
+		432: "Paladín",
+		321: "Dragontino",
+		968: "Lancero",
+	}
+	_, ok := profesiones[675]
+	fmt.Println(ok)
+
 }
