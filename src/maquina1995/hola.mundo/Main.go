93b736ff9f686d91c9ee4d3e258c670babc41d88
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-23 00:51:17 +0200

    Añadidos Punteros, avance de la wiki punto actual del video 4:21:32

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -2,31 +2,22 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type Magia struct {
-	mana   int
-	nombre string `required,max:"5"`
-}
-
-type MagiaNegra struct {
-	Magia
-	danno int
-}
-
 func main() {
-	magiaNegra := MagiaNegra{}
-	magiaNegra.mana = 10
-	magiaNegra.danno = 500
-	// {{10} 500}
-	fmt.Println(magiaNegra)
-	// 10
-	fmt.Println(magiaNegra.mana)
-	// 500
-	fmt.Println(magiaNegra.danno)
 
-	animalReflection := reflect.TypeOf(Magia{})
-	campo, _ := animalReflection.FieldByName("nombre")
-	fmt.Print(campo.Tag)
+	// numero1, numero2 := 1, 0
+	// division := numero1 / numero2
+	// fmt.Print(division)
+
+	var numero1 int = 1
+	var numero2 *int = &numero1
+	fmt.Println(numero1, numero2) // 1 0xc0000aa058
+
+	numero1 = 8
+
+	// Al usar el asterisco estamos reemplazando el
+	// valor de la memoria donde está almacenada por su valor real
+	fmt.Println(numero1, *numero2) // 8 8
+
 }
