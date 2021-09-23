526487592b79d6ee71737f4a7ac77091247d4fac
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-23 23:13:22 +0200

    Ejemplo de return multiple y correcto manejo de excepciones y adicion de mas contenido a la wiki

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -3,15 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	saludo := "Hola"
-	// nombre := "MaQuiNa"
-	// nombre2 := "MaQuina2"
+	division, error := dividir(5.0, 1.0)
 
-	saludarPorConsola(&saludo)
+	// Si error no es nulo quiere decir que hubo excepciones
+	if error != nil {
+		// imprimimos por consola el error
+		fmt.Print("Hubo un error mas info: ", error)
+		// paramos la ejecución del método
+		return
+	}
+	// Imprimimos el valor de la división
+	fmt.Print("El valor de la división es:", division)
 }
 
-func saludarPorConsola(saludo *string, nombres ...string) {
-	for _, nombre := range nombres {
-		fmt.Println(*saludo, nombre)
+// En una función podemos retornar mas de un valor
+// normalmente el 1º es el resultado de la función
+// y los segundos y sucesivos son errores por ejemplo
+func dividir(dividendo, divisor float64) (float64, error) {
+
+	if divisor == 0.0 {
+		// En vez de controlar el flujo con panic
+		// usamos la String creada a partir de fmt.Errorf
+		return 0.0, fmt.Errorf("No se puede dividir entre 0")
 	}
+
+	// retornamos el valor de la división y nil
+	// que indica que no hubo error
+	return dividendo / divisor, nil
 }
