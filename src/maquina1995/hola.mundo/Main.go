8e33360a86b7d0adf2d89888774ba8b67fe7be86
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-24 00:21:32 +0200

    Ejemplo de método + nuevas entradas en la wiki

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -2,32 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
-	division, error := dividir(5.0, 1.0)
-
-	// Si error no es nulo quiere decir que hubo excepciones
-	if error != nil {
-		// imprimimos por consola el error
-		fmt.Print("Hubo un error mas info: ", error)
-		// paramos la ejecución del método
-		return
-	}
-	// Imprimimos el valor de la división
-	fmt.Print("El valor de la división es:", division)
+type hechicero struct {
+	mana   int
+	nombre string
 }
 
-// En una función podemos retornar mas de un valor
-// normalmente el 1º es el resultado de la función
-// y los segundos y sucesivos son errores por ejemplo
-func dividir(dividendo, divisor float64) (float64, error) {
+func main() {
 
-	if divisor == 0.0 {
-		// En vez de controlar el flujo con panic
-		// usamos la String creada a partir de fmt.Errorf
-		return 0.0, fmt.Errorf("No se puede dividir entre 0")
+	mago := hechicero{
+		100,
+		"MaQuiNa",
 	}
 
-	// retornamos el valor de la división y nil
-	// que indica que no hubo error
-	return dividendo / divisor, nil
+	fmt.Println("Maná actual del mago: ", mago.mana)
+	// Aunque el struct no tenga este metodo hemos anclado esa función a ese tipo
+	mago.lanzarHechizo()
+	fmt.Println("Maná actual del mago: ", mago.mana)
+}
+
+// en este caso estamos "añadiendo" una función para poder ser invocada desde hechicero
+// de tal manera aunque los struct no puedan tener una función dentro como si de una clase
+// y un método en java serían podemos vincular una función a cierto struct
+
+// notese que si queremos que los cambios que hagamos a este struct se hagan
+// efectivos para en este caso la función main desde donde hemos pasado el struct
+// necesitamos marcar el tipo del parámetro con un *
+func (mago *hechicero) lanzarHechizo() {
+	mago.mana = mago.mana - 10
+	fmt.Println(mago.nombre, " Lanza Bola de fuego maná restante: ", mago.mana)
 }
