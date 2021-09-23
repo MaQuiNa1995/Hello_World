953af67e298d6649483ad9487d848501a0def95a
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-24 00:59:33 +0200

    Ejemplo con interfaces + entradas en la wiki 5:06:15 del video

--- src/maquina1995/hola.mundo/Main.go
+++ src/maquina1995/hola.mundo/Main.go
@@ -7,27 +7,42 @@ type hechicero struct {
 	nombre string
 }
 
-func main() {
+/*
+ Se define un comportamiento es decir en este caso se va a
+ escribir bytes en algun lugar ya sea un fichero una conexión TCP
+ solo nos interesa lo que entra y lo que sale de la función
+ esto es lo que denominamos "comportamiento"
 
-	mago := hechicero{
-		100,
-		"MaQuiNa",
-	}
-
-	fmt.Println("Maná actual del mago: ", mago.mana)
-	// Aunque el struct no tenga este metodo hemos anclado esa función a ese tipo
-	mago.lanzarHechizo()
-	fmt.Println("Maná actual del mago: ", mago.mana)
+ Lo que devuelva será (nº de bytes escritos y el posible
+	error que pueda producirse) respectivamente
+*/
+type Escritor interface {
+	Write([]byte) (int, error)
 }
 
-// en este caso estamos "añadiendo" una función para poder ser invocada desde hechicero
-// de tal manera aunque los struct no puedan tener una función dentro como si de una clase
-// y un método en java serían podemos vincular una función a cierto struct
+/*
+ Ahora solo necesitamos un struct que implemente esa interfaz
+ si vienes de otros lenguajes tipo java esperarías algo tipo
+ implements para implementar la interfaz pero en go , eso se hace
+ creando un método con la misma firma que el método de la interfaz
+ que acepte este struct
+*/
+type EscritorConsola struct{}
 
-// notese que si queremos que los cambios que hagamos a este struct se hagan
-// efectivos para en este caso la función main desde donde hemos pasado el struct
-// necesitamos marcar el tipo del parámetro con un *
-func (mago *hechicero) lanzarHechizo() {
-	mago.mana = mago.mana - 10
-	fmt.Println(mago.nombre, " Lanza Bola de fuego maná restante: ", mago.mana)
+/*
+ Aqui definimos la implementación de la interfaz
+*/
+func (escritorConsola EscritorConsola) Write(bytesEscribir []byte) (int, error) {
+	bytesEscritos, error := fmt.Print(string(bytesEscribir))
+	return bytesEscritos, error
+}
+
+func main() {
+
+	// De tal manera que puedo sustituir EscritorConsola{} por por ejemplo
+	// EscritorArchivo{} ya que ese tambien implementaría la interfaz Escritor
+	// porque cumple el contrato(método) de la misma
+	var escritor Escritor = EscritorConsola{}
+	escritor.Write([]byte("Hola mundo"))
+
 }
